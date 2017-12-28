16ab30aa5ca088bc5630fab0524757ecc19316cf
wolfstark <1038999405@qq.com>
2017-12-28 15:44:17 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import App from 'vue'
+interface VueComponent {
+  render: () => any;
+}
+export default {
+  render() {}
+}
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/main.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "build.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["css-loader", "sass-loader"]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "ts-loader",
+            options: {
+              appendTsSuffixTo: [/\.ts\.vue$/],
+              appendTsxSuffixTo: [/\.tsx\.vue$/]
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".vue", ".json", "tsx", "ts"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: "#eval-source-map",
+  plugins: [new webpack.optimize.ModuleConcatenationPlugin()]
+};
+
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map";
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
+}
